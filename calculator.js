fdd746c82ff8e41ef0c1e926e6989aa783584dd2
jesus carrion <jesusdcarriong@gmail.com>
2022-03-16 15:07:54 -0300

    calculadora

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-let lastOperator = null;
+/*let lastOperator = null;
 let reset = false;
 let lastNumber = null; 
 
@@ -19,10 +19,10 @@ function inict() {
 function processText(text){
     if(isNaN(text)){
         if(text == "c"){
-            document.getElementById("calculator").value =" ";
+            document.getElementById("calculator").value ="";
             reset = false;
         }else if(text == "ce"){
-            document.getElementById("calculator").value = " ";
+            document.getElementById("calculator").value = "";
             reset = false;
             lastOperator = null;
             lastNumber = null;
@@ -59,4 +59,99 @@ function processText(text){
             document.getElementById("calculator").value += text;
         }
     }
+}*/
+const buttonNumeros = document.getElementsByName('dataNumber');
+const buttonOpera = document.getElementsByName('dataOpera');
+const buttonIgual = document.getElementsByName('dataIgual')[0];
+const buttonDelete = document.getElementsByName('dataDelete')[0];
+let result = document.getElementById('result');
+let opeActual = '';
+let opeAnterior = '';
+let operacion = undefined;
+
+buttonNumeros.forEach(function(button){
+    button.addEventListener('click', function(){
+        agregarNumero(button.innerText);
+        
+
+    })
+    
+});
+
+buttonOpera.forEach(function(button){
+    button.addEventListener('click',function(){
+        selectOperacion(button.innerText);
+        
+    })
+
+});
+
+buttonIgual.addEventListener('click', function(){
+    calcular();
+    actualizarDisplay();
+
+});
+
+function selectOperacion(op){
+    if(opeActual === '') return;
+    if(opeAnterior !== ''){
+        calcular()
+    }
+        operacion = op.toString();
+        opeAnterior = opeActual;
+        opeActual = '';
 }
+function calcular(){
+    let calculo;
+    const anterior = parseFloat(opeAnterior);
+    const actual = parseFloat(opeActual);
+    if(isNaN(anterior) || isNaN(actual)) return;
+    switch(operacion){
+        case '+':
+            calculo = anterior + actual;
+            break;
+        case '-':
+            calculo = anterior - actual;
+            break;
+        case 'x':
+            calculo = anterior * actual;
+            break;
+        case '/':
+            calculo = anterior / actual;
+            break;
+        default:
+            return;
+    }
+    opeActual = calculo;
+    operacion = undefined;
+    opeAnterior = '';
+}
+
+buttonDelete.addEventListener('click', function(){
+    clear();
+    actualizarDisplay();
+});
+
+function agregarNumero(num){
+    opeActual = opeActual.toString() + num.toString();
+    actualizarDisplay();
+}
+function clear(){
+    opeActual = '';
+    opeAnterior = '';
+    operacion ='';
+}
+function actualizarDisplay(){
+    result.value = opeActual;
+}
+
+clear ();
+
+
+
+
+
+
+
+
+
