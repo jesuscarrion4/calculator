5a2870b847dda2e841f5f4f408a640f314634db0
jesus carrion <jesusdcarriong@gmail.com>
2022-03-30 23:16:06 -0300

    librerias

---
+++ js/sweealert.js
@@ -0,0 +1,10 @@
+swal.fire({
+    title: '¡Bienvenido!',
+    text: 'Espero que te guste mi calculadora',
+    icon: 'info',
+    confirmButtonText: 'Aceptar',
+    confirmButtonColor: 'green',
+    padding: '2rem',
+    backdrop: true,
+    timer: 5000,
+})
